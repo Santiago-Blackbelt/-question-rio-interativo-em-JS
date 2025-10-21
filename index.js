46e55efdbd676c2288263fe70bf5e2adea0b1040
Vinicius Santiago <pedrosanntiago@hotmail.com.br>
2025-10-20 23:12:27 -0300

    feat: questionário interativo em JS

---
+++ index.js
@@ -0,0 +1,27 @@
+const texto = window.document.getElementById('texto1');
+
+const botao = window.document.getElementById('botao1');
+
+const input = window.document.getElementById('texto');
+
+botao.addEventListener('click',adicionarTarefa);
+
+function adicionarTarefa(){
+    const textodigitado = input.value.trim();
+    if(textodigitado === '') return;
+
+    const item = window.document.createElement('li');
+
+    item.textContent = textodigitado;
+
+    texto.appendChild(item);
+    input.value = '';
+}
+
+/* Aperta o botão com o enter e não só com o cursor */
+input.addEventListener('keypress', (e) => {
+  if (e.key === 'Enter') {
+    e.preventDefault(); // ⛔ impede o reload da página
+    adicionarTarefa();
+  }
+});
